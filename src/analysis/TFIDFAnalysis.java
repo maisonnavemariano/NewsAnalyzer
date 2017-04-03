5c5394f120a261b3fe5bf8ab51678404b5330317
maiso <maisonnavemariano@gmail.com>
2017-04-03 14:54:38 -0300

    Jama library

---
+++ src/analysis/TFIDFAnalysis.java
@@ -0,0 +1,15 @@
+package analysis;
+
+import java.util.List;
+
+import Jama.Matrix;
+import components.Document;
+
+public class TFIDFAnalysis {
+	public Matrix computeTFIDF(List<Document> list){
+		return null;
+	}
+	public Matrix documentSimilarity(List<Document> list){
+		return null;
+	}
+}
